9f96db9b114c868fb4f9503de4be2f924094c28e
Jann <janncarlomoreno@gmail.com>
2019-03-31 10:28:22 -0500

    Promises

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,53 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll= new Promise(function(todoBien, todoMal){
+    //setInterval
+    //setTimeout
+    setTimeout(function(){
+        //luego de 3 segundos
+        //todoBien();
+        todoBien("se acabo el tiempo");
+    }, 5000);
+    //todoBien();
+})
+
+const getUser= new Promise(function(todoBien, todoMal){
+    //setInterval
+    //setTimeout
+    setTimeout(function(){
+        //luego de 3 segundos
+        //todoBien();
+        todoBien("se acabo el tiempo 3");
+    }, 3000);
+    //todoBien();
+})
+
+/*getUser
+    .then(function(){
+        console.log("todo esta bien en la vida");
+    })
+    .catch(function(message){
+        console.log(message);
+    })*/
+
+Promise.all([
+    getUser, getUserAll
+])
+.then(function(message){
+    console.log(message);
+})
+.catch(function(message){
+    console.log(message);
+})
+
+Promise.race([
+    getUser, getUserAll
+])
+.then(function(message){
+    console.log(message);
+})
+.catch(function(message){
+    console.log(message);
+})
